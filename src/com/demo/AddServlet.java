720328fd58f10ca236089b56079fbfb92fe346b8
kesavan-77 <kesavanvel77@gmail.com>
2025-04-05 21:30:23 +0530

    session and cookie storage

--- src/com/demo/AddServlet.java
+++ src/com/demo/AddServlet.java
@@ -5,18 +5,35 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		String name = req.getParameter("name");
 		String password = req.getParameter("password");
-		req.setAttribute("name", name);
-		req.setAttribute("password", password);
-//		RequestDispatcher dispatch = req.getRequestDispatcher("result");
-//		dispatch.forward(req, res);
+		
+		//session storage
+		HttpSession session = req.getSession();
+		session.setAttribute("name", name);
+		session.setAttribute("password", password);
+		
+		int num1 = 128;
+		int num2 = 128;
+		System.out.println(num1 == num2);
+		
+		//cookies storage
+		res.addCookie(new Cookie("name",name));
+		res.addCookie(new Cookie("password",password));
+		
 		res.sendRedirect("result");
 	}
 }
--- src/com/demo/ResultServlet.java
+++ src/com/demo/ResultServlet.java
@@ -5,15 +5,29 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ResultServlet extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-		RequestDispatcher dispatch = req.getRequestDispatcher("result.html");
-		dispatch.include(req, res);
-//		PrintWriter out = res.getWriter();
-//		out.println("vandthuten "+ req.getAttribute("name") + " your password: " + req.getAttribute("password")  + " senthuruchu");
+		
+		//session storage
+		HttpSession session = req.getSession();
+		String name = (String) session.getAttribute("name");
+		String password = (String) session.getAttribute("password");
+		
+		//cookies
+		Cookie cookie[] = req.getCookies();
+		for(Cookie c : cookie) {
+			System.out.println(c.getName() + "---" + c.getValue());
+		}
+		
+//		RequestDispatcher dispatch = req.getRequestDispatcher("result.html");
+//		dispatch.include(req, res);
+		PrintWriter out = res.getWriter();
+		out.println("vandthuten "+ name + " your password: " + password  + " senthuruchu");
 	}
 }
